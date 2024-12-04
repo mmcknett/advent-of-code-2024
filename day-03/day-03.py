79b03039c6141f0fee0f1d2bbb0d14363ae7bb53
Matt McKnett <mmcknett@gmail.com>
2024-12-03 20:41:42 -0800

    Day 3 part 2 complete

--- day-03/day-03.py
+++ day-03/day-03.py
@@ -11,12 +11,35 @@ def day3p1():
   assert answer == 189527826 # First try
 
 def mul(expr: str):
-  if not expr.startswith("mul(") and expr.endswith(")"):
+  if not is_mul(expr):
     raise ValueError(f"Didn't get a mul, got '{expr}'")
   stripped = expr[4:-1]
   a, b = stripped.split(",")
   return int(a) * int(b)
 
+def is_mul(expr:str):
+  return expr.startswith("mul(") and expr.endswith(")")
+
+def day3p2():
+  matches = re.findall('mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)', input)
+  exprs_to_sum = []
+  enabled = True
+  for m in matches:
+    match m:
+      case "don't()": enabled = False
+      case "do()": enabled = True
+      case _:
+        if is_mul(m) and enabled:
+          exprs_to_sum.append(m)
+        elif not is_mul(m):
+          raise ValueError(f"Didn't expect {m}")
+  
+  evaluated = (mul(expr) for expr in exprs_to_sum)
+  answer = sum(evaluated)
+  print(f"Part 2: {answer}")
+  assert answer == "63013756" # First try
+
 
 if __name__ == "__main__":
   day3p1()
+  day3p2()
